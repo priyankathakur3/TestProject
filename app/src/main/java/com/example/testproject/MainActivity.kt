41f168827d68a422b6f72079bfb419206f58a76a
Priyanka Thakur <priyankathakur7375@gmail.com>
2023-07-13 15:49:19 +0530

    Learn Box and Column

---
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.testproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.testproject.ui.theme.TestProjectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestProjectTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Box(modifier = Modifier.fillMaxSize()) {
+
+                        Column(modifier = Modifier.align(alignment = Alignment.Center)) {
+                            
+                            Text(text = "Good", style = TextStyle(fontWeight = FontWeight.ExtraBold))
+                            Text(text = "Great hgygd jkags")
+
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TestProjectTheme {
+        Greeting("Android")
+    }
+}
